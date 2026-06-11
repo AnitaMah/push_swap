5d89fa74b09d22ac77d524c55619a9c922d00b87
AnitaMah <140920875+AnitaMah@users.noreply.github.com>
2026-06-11 15:55:49 +0200

    Add untils.c utility file

---
+++ stack_sorting/untils_sort/untils.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   untils.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmakhov <anmakhov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/11 15:06:00 by anmakhov          #+#    #+#             */
+/*   Updated: 2026/06/11 15:06:01 by anmakhov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../push_swap.h"
+
+/* Placeholder utility functions for future sorting utilities */
+/* Currently empty - can be extended with additional helper functions */
